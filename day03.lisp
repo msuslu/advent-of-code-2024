6a7bf3054da42160453e42c7332bd7aef017002d
Mehmet Suslu <>
2024-12-31 13:35:52 +0300

    fix day 3 part 2

--- day03.lisp
+++ day03.lisp
@@ -3,20 +3,27 @@
 
 (defun solve-puzzle (filename)
       (with-open-file (stream filename)
+        ;; Part1
         (let ((sum 0))
-          ;; Part1
-          (ppcre:do-register-groups ((#'parse-integer val1 val2)) ("mul\\((\\d+),(\\d+)\\)" (alexandria:read-file-into-string stream) nil :sharedp t)
+          (ppcre:do-register-groups ((#'parse-integer val1 val2))
+              ("mul\\((\\d+),(\\d+)\\)" (alexandria:read-file-into-string stream) nil :sharedp t)
             (incf sum (* val1 val2)))
           (print sum))
         ;; Part2
         (let ((sum 0)
               (current-state 'do)
-              (content (alexandria:read-file-into-string stream)))
-          (ppcre:do-register-groups (state (#'parse-integer val1 val2)) ("(?:(do|don't)\\(\\))?.*?mul\\((\\d+),(\\d+)\\)" content nil :sharedp t)
-            (format t "~%~a ~a ~a" state val1 val2)
-            (when state
-              (cond ((string= state "do") (setf current-state 'do))
-                    ((string= state "don't") (setf current-state 'dont))))
-            (when (eq current-state 'do)
-              (incf sum (* val1 val2))))
+              (content (alexandria:read-file-into-string stream))
+              (scanner (ppcre:create-scanner '(:alternation
+                                               (:register "do()")
+                                               (:register "don't()")
+                                               (:sequence
+                                                (:register "mul") #\(
+                                                (:register (:greedy-repetition 1 nil :digit-class)) #\,
+                                                (:register (:greedy-repetition 1 nil :digit-class)) #\))) :multi-line-mode t)))
+          (ppcre:do-register-groups (do dont mul (#'parse-integer val1 val2))
+              (scanner content nil :sharedp t)
+            (cond (do (setf current-state 'do))
+                  (dont (setf current-state 'dont))
+                  (mul (when (eq current-state 'do)
+                         (incf sum (* val1 val2))))))
           (print sum))))
